a79143851b2f58a8b2dd40815a2fcc4ceb0984fd
Caleb Cox <caleb@caleb-cox.com>
2017-10-26 12:57:29 -0400

    Added dashboard ejs and styles

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,8 @@
 var express      = require('express'),
     router       = express.Router();
 
+    router.get("/", function(req, res) {
+        res.render("dashboard");
+    });
 
-
-
-
-    module.exports = router;
+module.exports = router;
