675ccce75bd9b50c88e9485196e253b5be5932bf
Caleb Cox <caleb@caleb-cox.com>
2017-10-26 21:47:43 -0400

    updated dashboard ejs

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,20 @@
 var express      = require('express'),
     router       = express.Router();
 
-    router.get("/", function(req, res) {
-        res.render("dashboard");
-    });
+// INDEX
+router.get("/", function(req, res) {
+    res.render("dashboard/index");
+});
+
+// CREATE
+router.post("/", function(req, res) {
+    // get data from form
+    // add new domain to DB
+});
+
+// NEW
+router.get("/new", function(req, res) {
+    res.send("New domain form here!");
+});
 
 module.exports = router;
