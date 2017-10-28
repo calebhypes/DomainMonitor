7f3793f8912f4b57f6c0919e65fadcd76ae5b2ea
Caleb Cox <caleb@caleb-cox.com>
2017-10-28 17:39:55 -0400

    added register logic

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,29 @@ router.get("/", function(req, res) {
 
 // Authentication routes here
 
+// show register form
+router.get("/register", function(req, res) {
+    res.render("register");
+});
 
+// sign up logic
+router.post("/register", function (req, res) {
+    var newUser =  new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log("Error occurred while creating account!");
+            return res.redirect("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            console.log("Account created successfully!");
+            res.redirect("/dashboard");
+        });
+    });
+});
 
-
+// login form
+router.get("/login", function(req, res) {
+    res.render("login");
+});
 
 module.exports = router;
